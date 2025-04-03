86d62c7e6eec8e1ebbd846510aa988e1cf3dfee1
Lm <135152837+Lm260@users.noreply.github.com>
2025-04-02 22:43:20 -0400

    Correção

--- upsh.sh
+++ upsh.sh
@@ -1,1 +1,2 @@
-curl https://raw.githubusercontent.com/Lm260/HutaoBot-MD/refs/heads/main/src/js/fastgenyt/lib/server/request.js > src/js/fastgenyt/lib/server/request.js
+curl https://raw.githubusercontent.com/Lm260/HutaoBot-MD/refs/heads/main/src/js/fastgenyt/lib/server/request.js > src/js/fastgenyt/lib/server/request.js &&
+curl https://raw.githubusercontent.com/Lm260/HutaoBot-MD/refs/heads/main/assets/users/family.json > assets/users/family.json
