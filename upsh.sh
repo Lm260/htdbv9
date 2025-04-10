57536b73e4db8ebb4d29ca1fd10b9245920b8328
Lm <135152837+Lm260@users.noreply.github.com>
2025-04-10 17:08:57 -0400

    Mm

--- upsh.sh
+++ upsh.sh
@@ -1,2 +1,1 @@
-curl https://raw.githubusercontent.com/Lm260/HutaoBot-MD/refs/heads/main/index.js > index.js &&
-curl https://raw.githubusercontent.com/Lm260/HutaoBot-MD/refs/heads/main/dono/message.js > dono/message.js
+curl https://raw.githubusercontent.com/Lm260/HutaoBot-MD/refs/heads/main/index.js > index.js
