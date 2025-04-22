4dfbabd4fa4640b9d5b36ad48d744fe9035f6017
Lm <135152837+Lm260@users.noreply.github.com>
2025-04-21 22:28:16 -0400

    Up
    Date

--- upsh.sh
+++ upsh.sh
@@ -1,1 +1,2 @@
 curl https://raw.githubusercontent.com/Lm260/HutaoBot-MD/refs/heads/main/src/js/functions.js > src/js/functions.js
+curl https://raw.githubusercontent.com/Lm260/HutaoBot-MD/refs/heads/main/dono/settings/imglinks.json > dono/settings/imglinks.json
